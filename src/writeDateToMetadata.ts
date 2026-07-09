848260ee8aeef08b2bcef095d233a5719b0344b9
cptSwing <jens@jbrandenburg.de>
2026-07-09 15:07:08 +0200

    rename

--- src/writeDateToMetadata.ts
+++ src/writeDateToMetadata.ts
@@ -1,10 +1,10 @@
 import type { ExifTool } from 'exiftool-vendored';
 
-const writeMetadata = async (exifToolInst: ExifTool, path: string) => {
+const writeDateToMetadata = async (exifToolInst: ExifTool, path: string) => {
     await exifToolInst.write('photo.jpg', {
         XPComment: 'Amazing sunset!',
         Copyright: '© 2024 Your Name',
     });
 };
 
-export default writeMetadata;
+export default writeDateToMetadata;
